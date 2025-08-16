9e3420d9d33b04e43f9c496c61e6a5fca99b053f
abidhossain384 <abidhossain384@gmail.com>
2025-08-16 16:34:41 +0600

    Module 6 Project

--- app/classes/VehicleActions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-interface VehicleActions {
-   public function addVehicle($data);
-   public function editVehicle($id, $data);
-   public function deleteVehicle($id);
-   public function getVehicles();
-}
